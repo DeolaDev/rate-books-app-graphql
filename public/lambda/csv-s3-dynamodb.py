66a54ef07591636d7da9a68ad455a2e636b76d30
DeolaDev <deoladeveloper@gmail.com>
2023-03-06 21:42:26 -0500

    updated link in README

--- public/lambda/csv-s3-dynamodb.py
+++ public/lambda/csv-s3-dynamodb.py
@@ -1,9 +1,11 @@
+import json
 import boto3
+from datetime import datetime
 
 s3_client = boto3.client("s3")
 dynamodb_client = boto3.resource("dynamodb")
 
-table = dynamodb_client.Table("my-table-name")
+table = dynamodb_client.Table("S3CsvBook-bywzmapc7be3rd5q2ekb3flyvu-staging")
 
 def lambda_handler(event, context):
     bucketname = event['Records'][0]['s3']['bucket']['name']
@@ -14,8 +16,9 @@ def lambda_handler(event, context):
     Books = data.split("\n")
     
     for book in Books:
+        print(book)
+        print(datetime.now())
         book_entry = book.split(",")
-        
         # add entry to dynamodb
         try:
             table.put_item(
@@ -24,10 +27,10 @@ def lambda_handler(event, context):
                     "title"     : book_entry[1],
                     "author"    : book_entry[2],
                     "rating"    : book_entry[3],
-                    "createdAt" : book_entry[4],
-                    "updatedAt" : book_entry[5]
+                    "createdAt" : datetime.now(),
+                    "updatedAt" : datetime.now()
                     
                 }
             )
         except Exception as e:
-            print("Done")
+            print("Error")
